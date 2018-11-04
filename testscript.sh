ff6066e4ac876b9ab16bac76c8774663e9cbd75a
Cyprien Dcunha <dcunha.cyprien@gmail.com>
2018-11-04 13:40:04 +0530

    testscript: check only files modified in last commit instead of all

--- testscript.sh
+++ testscript.sh
@@ -6,40 +6,52 @@ YELLOW="\e[93m"
 
 # Checks
 echo -e "${YELLOW}(1/4) Checking for newlines at end of all file${WHITE}"
-for f in $(find -name "*.cpp" -o -name "*.py"); do
-    if ! [ -z "$(tail -c 1 "$f")" ]
+for f in $(git diff-tree --no-commit-id --name-only -M -r HEAD); do
+    if ([[ $f == *.cpp ]] || [[ $f == *.py ]])
     then
-        echo -e "${RED}No newline at end of file - $f${WHITE}"
-        exit 1
+        if ! [ -z "$(tail -c 1 "$f")" ]
+        then
+            echo -e "${RED}No newline at end of file - $f${WHITE}"
+            exit 1
+        fi
     fi
 done
 
 echo -e "${YELLOW}(2/4) Checking for trailing whitespaces${WHITE}"
-for f in $(find -name "*.cpp" -o -name "*.py"); do
-    if grep -rli '[[:blank:]]$' $f
+for f in $(git diff-tree --no-commit-id --name-only -M -r HEAD); do
+    if ([[ $f == *.cpp ]] || [[ $f == *.py ]])
     then
-        echo -e "${RED}Trailing whitespaces - $f${WHITE}"
-        exit 1
+        if grep -rli '[[:blank:]]$' $f
+        then
+            echo -e "${RED}Trailing whitespaces - $f${WHITE}"
+            exit 1
+        fi
     fi
 done
 
 echo -e "${YELLOW}(3/4) Compiling python files${WHITE}"
-for f in $(find -name "*.py"); do
-    echo "Compiling - $f"
-    if ! python3 -c "import py_compile; py_compile.compile('$f', doraise=True)"
+for f in $(git diff-tree --no-commit-id --name-only -M -r HEAD); do
+    if [[ $f == *.py ]]
     then
-        echo -e "${RED}Error compiling file - $f${WHITE}"
-        exit 1
+        echo "Compiling - $f"
+        if ! python3 -c "import py_compile; py_compile.compile('$f', doraise=True)"
+        then
+            echo -e "${RED}Error compiling file - $f${WHITE}"
+            exit 1
+        fi
     fi
 done
 
 echo -e "${YELLOW}(4/4) Compiling C++ files${WHITE}"
-for f in $(find -name "*.cpp"); do
-    echo "Compiling - $f"
-    if ! g++ -pipe -Wfatal-errors $f
+for f in $(git diff-tree --no-commit-id --name-only -M -r HEAD); do
+    if [[ $f == *.cpp ]]
     then
-        echo -e "${RED}Error compiling file - $f${WHITE}"
-        exit 1
+        echo "Compiling - $f"
+        if ! g++ -pipe -Wfatal-errors $f
+        then
+            echo -e "${RED}Error compiling file - $f${WHITE}"
+            exit 1
+        fi
     fi
 done
 
